ec940875dcce68ead2f6a17946e31b1cac39872c
LAPTOP-CB2DOMS8\juits <juitse1108@gmail.com>
2023-05-18 17:45:49 +0800

    HomeItem

--- React/src/components/main/HomeItem.jsx
+++ React/src/components/main/HomeItem.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import './HomeItem.css'
 
 function HomeItem() {
-    return ( 
+    return (
         <div className='homeContainer'>
-            <div className='card'>
-                
+            <div className='cardBox'>
+                <div className='imgBox'>
+                    <img
+                        src="./src/templates/image/img1.jpg"
+                        alt=""
+                        className='cardImage'
+                    />
+                </div>
+                <div className='textBox'>
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                    <p>4</p>
+                </div>
             </div>
         </div>
     );
