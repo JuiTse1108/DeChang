b85e6470defa445a26f4dc967fb9cc21f5e8def8
LAPTOP-CB2DOMS8\juits <juitse1108@gmail.com>
2023-06-07 03:47:09 +0800

    location

--- React/src/components/pages/location/LocationItem.jsx
+++ React/src/components/pages/location/LocationItem.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import Geocode from "react-geocode";
-import mapData from "../../../assets/data/map.json"
+import Geocode from 'react-geocode';
 import './LocationItem.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
@@ -11,7 +10,7 @@ function LocationItem() {
     const apiKey = import.meta.env.VITE_GOOGLEMAP_API_KEY;
     Geocode.setApiKey(apiKey);
     Geocode.setLanguage('zh-TW');
-    Geocode.setRegion("tw");
+    Geocode.setRegion('tw');
 
     const center = {
         lat: 23.97565,
@@ -23,29 +22,45 @@ function LocationItem() {
         streetViewControl: false,
         disableDefaultUI: true
     }
-    const aikey = import.meta.env.VITE_OPENAI_API_KEY;
-    console.log(aikey)
+    //先取得map.json
+    const fetchData = async () => {
+        const response = await fetch('../../src/assets/data/map.json')
+        const data = await response.json()
+        return data.records
+    }
+
+    const [data, setData] = useState([]);
+    const [siteName, setSiteName] = useState('');
+    const [address, setAddress] = useState('');
+    const [telephone, setTelephone] = useState('');
+
+    useEffect(() => {
+        async function getData() {
+            const records = await fetchData();
+            setData(records);
+        }
+        getData();
+    })
+
     return (
         <React.Fragment>
             <div className='mapContainer'>
                 <div className='storeInfo'>
                     <div className='storeBox'>
-                        <React.Fragment>
-                            <div className='storeItem'>
-                                <i className="bi bi-shop"></i>Name : <span className="siteItem">{location.sitename}</span></div>
-                            <div className='storeItem'>
-                                <i className="bi bi-pin-map-fill"></i>Address : <span className="siteItem">{location.address}</span></div>
-                            <div className='storeItem'>
-                                <i className="bi bi-telephone-fill"></i>Phone : <span className="siteItem">{location.telephone}</span></div>
-                            <div className='storeItem'>
-                                <i className="bi bi-clock-fill"></i>Opening Hours
-                                <div className='dayBox'>
-                                    <div className='day'>Mon ~ Fri<span className='time'>10:30 ~ 22:00</span></div>
-                                    <div className='day'>Sat<span className='time'>10:00 ~ 23:00</span></div>
-                                    <div className='day'>Sun<span className='time'>10:00 ~ 21:30</span></div>
-                                </div>
+                        <div className='storeItem'>
+                            <i className='bi bi-shop'></i>Name : <span className='siteItem'>{siteName}</span></div>
+                        <div className='storeItem'>
+                            <i className='bi bi-pin-map-fill'></i>Address : <span className='siteItem'>{address}</span></div>
+                        <div className='storeItem'>
+                            <i className='bi bi-telephone-fill'></i>Phone : <span className='siteItem'>{telephone}</span></div>
+                        <div className='storeItem'>
+                            <i className='bi bi-clock-fill'></i>Opening Hours
+                            <div className='dayBox'>
+                                <div className='day'>Mon ~ Fri<span className='time'>10:30 ~ 22:00</span></div>
+                                <div className='day'>Sat<span className='time'>10:00 ~ 23:00</span></div>
+                                <div className='day'>Sun<span className='time'>10:00 ~ 21:30</span></div>
                             </div>
-                        </React.Fragment>
+                        </div>
                     </div>
                     <div className='mapBox'>
                         <LoadScript googleMapsApiKey={apiKey}>
@@ -56,6 +71,25 @@ function LocationItem() {
                                 id='map'
                                 onLoad={() => { }}
                             >
+                                <Marker
+                                    position={{ lat: center.lat, lng: center.lng }}>
+                                </Marker>
+                                {data.map((location, index) => {
+                                    return (
+                                        <Marker
+                                            key={index}
+                                            position={{
+                                                lat: parseFloat(location.latitude),
+                                                lng: parseFloat(location.longitude)
+                                            }}
+                                            onClick={() => {
+                                                setSiteName(location.sitename);
+                                                setAddress(location.address);
+                                                setTelephone(location.telephone);
+                                            }}
+                                        />
+                                    )
+                                })}
                             </GoogleMap>
                         </LoadScript>
                     </div>
